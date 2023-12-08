5cd96eb24c1749292048c7a34d860dda1c4007a5
rohan0420 <skrohan0420@gmail.com>
2023-12-08 18:29:56 +0530

    react cards

--- mernapp/src/components/Card.js
+++ mernapp/src/components/Card.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Card() {
     return (
 
-        <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-            <img src="..." className="card-img-top" alt="..." />
+        <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "fitContent" }}>
+            <img src="https://source.unsplash.com/random/100×100/?burger" className="card-img-top" alt="..." style={{"height":"200px"}}/>
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
--- mernapp/src/components/Carousel.js
+++ mernapp/src/components/Carousel.js
@@ -2,29 +2,35 @@ import React from 'react'
 
 function Carousel() {
     return (
-        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" >
             <div className="carousel-indicators">
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
             <div className="carousel-inner">
+                <div className='carousel-captions' style={{"position":"relative","top":"230px","zIndex":"100","width":"400px","left":"50%","transform":"translate(-50%)","height":"0px"}}>
+                    <form className="d-flex">
+                        <input className="form-control me-2" style={{'backgroundColor': '#fff', 'color': '#000', 'border': 'none'}} type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-success" type="submit">Search</button>
+                    </form>
+                </div>
                 <div className="carousel-item active">
-                    <img src="https://source.unsplash.com/random/600×300/?burger" className="d-block w-100" alt="..." style={{ "maxHeight": "500px" }} />
+                    <img src="https://source.unsplash.com/random/300×600/?burger" className="d-block w-100" alt="..." style={{ "maxHeight": "500px" }} />
                     <div className="carousel-caption d-none d-md-block">
                         <h5>First slide label</h5>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://source.unsplash.com/random/600×300/?pizza" className="d-block w-100" alt="..." style={{ "maxHeight": "500px" }} />
+                    <img src="https://source.unsplash.com/random/300×600/?pizza" className="d-block w-100" alt="..." style={{ "maxHeight": "500px" }} />
                     <div className="carousel-caption d-none d-md-block">
                         <h5>Second slide label</h5>
                         <p>Some representative placeholder content for the second slide.</p>
                     </div>
                 </div>
                 <div className="carousel-item">
-                    <img src="https://source.unsplash.com/random/600×300/?rice" className="d-block w-100" alt="..." style={{ "maxHeight": "500px" }} />
+                    <img src="https://source.unsplash.com/random/300×600/?rice" className="d-block w-100" alt="..." style={{ "maxHeight": "500px" }} />
                     <div className="carousel-caption d-none d-md-block">
                         <h5>Third slide label</h5>
                         <p>Some representative placeholder content for the third slide.</p>
